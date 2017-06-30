8d12f4ff65191cd437a19971b689c23c2068fe15
MithunBurman7 <mithunburman7@gmail.com>
2017-06-30 15:03:54 +0530

    Assignment11.2

---
+++ src/AssignmentDemo/DivideJava.java
@@ -0,0 +1,18 @@
+package AssignmentDemo;
+
+import java.util.Scanner;
+
+public class DivideJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Scanner in = new Scanner(System.in);//Scanner for Input
+       System.out.println("Input the numerator: ");
+       int num1 = in.nextInt();
+       
+       int denominator = 0;
+       
+	   System.out.println(num1/denominator);
+	   }
+
+}
